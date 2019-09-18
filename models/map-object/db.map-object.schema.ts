d652a2293e906dd3ebaa1568a04c61822e487ab5
Lucas Moesch <lucasmoesch@live.de>
2019-09-18 16:45:38 +0000

    Added database models

---
+++ models/map-object/db.map-object.schema.ts
@@ -0,0 +1,23 @@
+import { Schema } from 'mongoose';
+
+export const MapObjectSchema: Schema = new Schema({
+    position: { x: Number, y: Number },
+    uid: {type: String, required: true, unique: true},
+    name: {type: String},
+    type: {type: String, required: true},
+    meta: {
+        size: {type: Number},
+        description: {type: String},
+        wounded: {type: Number},
+        callsign: {type: String},
+        subtype: {type: Number},
+        tracked: {type: Boolean},
+        medics: {type: Number},
+        technitians: {type: Number},
+        scientists: {type: Number}
+    }
+
+    // email: { type: String, required: true, unique: true },
+    // firstName: { type: String, required: true },
+    // lastName: { type: String, required: true },
+});
